72adeb07390ab51f289e0875a741c5d0f7bf4648
Rostiks52 <ksyu_05@mail.ru>
2021-08-20 20:50:44 +0300

    Modified tests on JUnit4

--- src/test/java/ru/netology/service/CashbackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static  org.junit.Assert.*;
 
 public class CashbackServiceTest {
     CashbackService service = new CashbackService();
@@ -11,20 +11,20 @@ public class CashbackServiceTest {
     public void shouldCalculateIfAmount1000() {
         int expected = 0;
         int actual = service.remain(1000);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldCalculateIfAmount999() {
         int expected = 1;
         int actual = service.remain(999);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldCalculateIfAmount1001(){
         int expected = 999;
         int actual = service.remain(1001);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
